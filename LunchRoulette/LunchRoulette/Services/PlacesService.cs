307353463a50696e1aea45c1a70f4a0c116db447
Peter Gaard <pgaard@gmail.com>
2016-12-25 17:07:40 -0600

    basic map and search working

---
+++ LunchRoulette/LunchRoulette/Services/PlacesService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using LunchRoulette.Models;
+using Newtonsoft.Json;
+using PCLAppConfig;
+
+namespace LunchRoulette.Services
+{
+    public class PlacesService
+    {
+        private readonly HttpClient client;
+
+        private readonly string googlePlacesApiKey;
+
+        private string Url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={0}&location={1},{2}&rankby=distance&type=restaurant";
+
+        public PlacesService()
+        {
+            this.client = new HttpClient();
+            googlePlacesApiKey = Config.GooglePlacesApiKey;
+        }
+
+        public async Task<Restaurants> GetRestaurants(double latitude = 44.9135599, double longitude = -93.21555699999999)
+        {
+            try
+            {
+                var response = await this.client.GetAsync(new Uri(String.Format(this.Url, this.googlePlacesApiKey, latitude, longitude)));
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    var restaurants = JsonConvert.DeserializeObject<Restaurants>(result);
+                    return restaurants;
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return null;
+        }
+    }
+}
---
+++ LunchRoulette/LunchRoulette/Views/LandingPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using LunchRoulette.Models;
+using LunchRoulette.Services;
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
+
+namespace LunchRoulette.Views
+{
+    using Xamarin.Forms;
+
+    public partial class LandingPage : ContentPage
+    {
+        private PlacesService service;
+
+        public ObservableCollection<Restaurant> Restaurants;
+
+        public LandingPage()
+        {
+            this.service = new PlacesService();
+            InitializeComponent();
+        }
+
+        public async Task UpdateMap()
+        {
+            var location = await GetCurrentPosition();
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromMiles(1)));
+        }
+
+        public async Task<Position> GetCurrentPosition()
+        {
+            try
+            {   
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                var location = await locator.GetPositionAsync(10000);
+                return new Position(location.Latitude, location.Longitude);
+            }
+            catch (Exception ex)
+            {
+                var geoCoder = new Geocoder();
+                var location = await geoCoder.GetPositionsForAddressAsync("4916 40th Ave S Minneapolis MN 55417");
+                return location.FirstOrDefault();
+            }
+        }
+
+        public async void Button_Clicked(object sender, EventArgs eventArgs)
+        {
+            var restaurants = await this.service.GetRestaurants(this.MyMap.VisibleRegion.Center.Latitude, this.MyMap.VisibleRegion.Center.Longitude);
+            if (restaurants != null && restaurants.results.Count > 0)
+            {
+                this.Restaurants = new ObservableCollection<Restaurant>(restaurants.results);
+
+                var rnd = new Random();
+                var randomRestaurant = restaurants.results[rnd.Next(0, restaurants.results.Count)];
+
+                this.Winner.Text = "Winner: " + randomRestaurant.name;
+                MyMap.Pins.Add(new Pin()
+                {
+                    Position =
+                        new Position(randomRestaurant.geometry.location.lat, randomRestaurant.geometry.location.lng),
+                    Label = randomRestaurant.name
+                });
+
+                chowList.ItemsSource = this.Restaurants;
+            }                        
+        }
+      
+        private async void Button_Map(object sender, EventArgs e)
+        {
+            await this.UpdateMap();
+        }
+    }
+}
